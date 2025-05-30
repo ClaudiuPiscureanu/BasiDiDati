ebfe6535839f9dc4e3ec08ff7cf72c762e78e296
ClaudiuPiscureanu <laurenticlaudiu.piscureanu@gmail.com>
2025-05-30 14:03:59 +0200

    aggiornamento entita

--- claudb.sql
+++ claudb.sql
@@ -31,7 +31,7 @@ CREATE TABLE cinema(
 --
 
 CREATE TABLE sala(
-    num_sala TINYINT NOT NULL AUTO_INCREMENT,  -- tinyint da 0 a 255 	Da 2^0-1 a 2^8-1 	1 byte 
+    num_sala TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,  -- tinyint da 0 a 255 	Da 2^0-1 a 2^8-1 	1 byte 
     nome_cinema VARCHAR(45) NOT NULL,
     PRIMARY KEY(num_sala),
     FOREIGN KEY (nome_cinema) REFERENCES cinema(nome_cinema)
@@ -43,7 +43,7 @@ CREATE TABLE sala(
 
 CREATE TABLE posto (
     id_posto VARCHAR(3) NOT NULL,
-    num_sala TINYINT NOT NULL,
+    num_sala TINYINT UNSIGNED NOT NULL,
     fila CHAR(1),
     num_posto TINYINT NOT NULL,
     PRIMARY KEY (id_posto),
@@ -58,7 +58,10 @@ CREATE TABLE posto (
 CREATE TABLE film(
     titolo_film VARCHAR(128) NOT NULL PRIMARY KEY,
     durata TINYINT UNSIGNED NOT NULL CHECK (durata > 0), -- Durata in minuti
-    CasaCinematografica VARCHAR(255) NOT NULL
+    produzione VARCHAR(255) NOT NULL,
+    cast_attori TINYINT NOT NULL,
+    FOREIGN KEY (produzione) REFERENCES produzione(nome_produzione) ON DELETE CASCADE, -- il comando on delete cascade, cancella tutti i film dopo aver eliminato la produzione
+    FOREIGN KEY (cast_attori) REFERENCES attore(id_attore) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 --
 -- Table structure for table `attore`
@@ -74,11 +77,21 @@ CREATE TABLE attore(
 --
 
 CREATE TABLE cast_attori(
+    id_cast TINYINT UNSIGNED NOT NULL AUTO_INCREMENT, -- The unsigned range is 0 to 255.
     titolo_film VARCHAR(128),
     id_attore TINYINT UNSIGNED NOT NULL,
-    PRIMARY KEY (titolo_film, id_attore),  
+    PRIMARY KEY (id_cast),  
     FOREIGN KEY (titolo_film) REFERENCES film(titolo_film) ON DELETE CASCADE, -- il comando on delete cascade, calcella tutti gli attori dopo aver eliminato il film
     FOREIGN KEY (id_attore) REFERENCES  attore(id_attore) ON DELETE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--
+-- Table structure for table `produzione`
+--
+
+CREATE TABLE produzione( --riferito alla casa di produzione del film
+    nome_produzione VARCHAR(128) NOT NULL,
+    PRIMARY KEY (nome_produzione),  
+    intro_produzione VARCHAR(255) 
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -109,10 +122,7 @@ CREATE TABLE prenotazione(
     -- data_ora_scadenza DATETIME, -- Nuovo campo per la scadenza della prenotazione
     -- data_ora_annullamento DATETIME, -- Nuovo campo per l'annullamento della prenotazione
     stato BOOLEAN DEFAULT NULL,  -- stato ENUM('in attesa', 'confermata', 'annullata') DEFAULT 'in attesa',
-    numero_carta VARCHAR(16),
-    intestatario VARCHAR(50),
-    cvv VARCHAR(3) NOT NULL, -- a livello client 
-    scadenza DATE,
+    ticket_pag VARCHAR(50),--verrà memorizzato unicamente un ticket e non le informazioni di pagamento per delegare tutta la logica annessa a programmi esterni 
     PRIMARY KEY(codice_prenotazione),
     FOREIGN KEY (id_proiezione) REFERENCES proiezione(id_proiezione),
     FOREIGN KEY (id_posto) REFERENCES posto(id_posto),
@@ -126,10 +136,11 @@ CREATE TABLE prenotazione(
 CREATE TABLE report(
     anno YEAR NOT NULL,
     mese TINYINT NOT NULL,
-    num_sala TINYINT NOT NULL,
+    num_sala TINYINT UNSIGNED NOT NULL,
+    nome_cinema VARCHAR(45) NOT NULL,
     perc_annulati DECIMAL(10,2) NOT NULL,
     perc_confermati DECIMAL(10,2) NOT NULL,
-    PRIMARY KEY (anno, mese, num_sala),
+    PRIMARY KEY (anno, mese, num_sala, nome_cinema),
     FOREIGN KEY (num_sala) REFERENCES sala(num_sala)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
